6103a9c79ad210ee7b19a7914fb627d506286a5b
JB4000 <jabi0002@stud.ek.dk>
2025-11-30 20:03:41 +0100

    Initial commit

---
+++ src/main/java/com/pug/car_rent_app/CarRentAppApplication.java
@@ -0,0 +1,13 @@
+package com.pug.car_rent_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pug/car_rent_app/CarRentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pug.car_rent_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
